23a66e34d7409e06daaf67cdbe53a0e9aefc6786
Ameesha Pratheep <ameeshapratheep@gmail.com>
2026-06-10 22:46:41 +0530

    fix: use VITE_ env vars for Vite

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,4 +1,4 @@
-function Login() {
-  return <h1 style={{color: 'white', textAlign: 'center', marginTop: '200px'}}>REACT IS WORKING</h1>
-}
-export default Login;
+import './Login.css'  // L must be capital
+import App from './App' // A must be capital  
+import firebase from './firebase' // f lowercase matches file
+import supabase from './supabase' // s lowercase matches file
