8757ab8b683115fd7e925c896af636af6e131ecd
Ameesha Pratheep <ameeshapratheep@gmail.com>
2026-06-13 08:57:59 +0530

    Add Firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBJ9TbxzAlRrlqdCdUzKo4haDNAwRWe_wo",
@@ -8,8 +8,8 @@ const firebaseConfig = {
   storageBucket: "instory-app-78ca9.firebasestorage.app",
   messagingSenderId: "889330601899",
   appId: "1:889330601899:web:1307cd33c5130ce4a58cee"
-};
+}
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,5 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        burgundy:'#800020',
-      },
-    },
-  },
-  safelist: [
-    'bg-burgundy', 'hover:bg-[#660018]', 'text-white', 'rounded-lg',
-    'shadow-md', 'w-full', 'max-w-sm', 'mx-auto', 'p-8', 'space-y-4',
-    'min-h-screen', 'flex', 'items-center', 'justify-center', 'border',
-    'border-gray-300', 'focus:border-burgundy', 'focus:ring-2', 'focus:ring-burgundy'
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
   plugins: [],
 }
